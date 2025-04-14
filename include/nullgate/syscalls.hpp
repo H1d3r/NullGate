fcd40fa727ba1bddc9733278a3eddfa05e4a04ac
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-04-14 22:15:09 +0200

    Add SCall which provides type safety and fixes issues like #2

--- include/nullgate/syscalls.hpp
+++ include/nullgate/syscalls.hpp
@@ -20,29 +20,40 @@ class syscalls {
   DWORD getSyscallNumber(uint64_t funcNameHash);
   uintptr_t getSyscallInstrAddr();
 
+  // We don't care that are right know there could be more on the stack, this
+  // is supposed to represent the number of args on the stack disregarding the
+  // added arguments
+  template <typename... Args> constexpr size_t getArgStackSize(Args &&...args) {
+    return sizeof...(args) <= 4 ? 0 : sizeof...(args) - 4;
+  }
+
 public:
   explicit syscalls();
 
   template <typename... Args>
   NTSTATUS Call(const std::string &funcName, Args... args) {
-    // We don't care that are right know there could be more on the stack, this
-    // is supposed to represent the number of args on the stack disregarding the
-    // added arguments
-    constexpr size_t argStackSize =
-        sizeof...(args) <= 4 ? 0 : sizeof...(args) - 4;
     return trampoline(getSyscallNumber(funcName), getSyscallInstrAddr(),
-                      argStackSize, std::forward<Args>(args)...);
+                      getArgStackSize(args...), std::forward<Args>(args)...);
   }
 
   template <typename... Args>
   NTSTATUS Call(uint64_t funcNameHash, Args... args) {
-    // We don't care that are right know there could be more on the stack, this
-    // is supposed to represent the number of args on the stack disregarding the
-    // added arguments
-    constexpr size_t argStackSize =
-        sizeof...(args) <= 4 ? 0 : sizeof...(args) - 4;
     return trampoline(getSyscallNumber(funcNameHash), getSyscallInstrAddr(),
-                      argStackSize, std::forward<Args>(args)...);
+                      getArgStackSize(args...), std::forward<Args>(args)...);
+  }
+
+  template <typename func, typename... Args>
+    requires std::invocable<func, Args...>
+  NTSTATUS SCall(const std::string &funcName, Args &&...args) {
+    return trampoline(getSyscallNumber(funcName), getSyscallInstrAddr(),
+                      getArgStackSize(args...), std::forward<Args>(args)...);
+  }
+
+  template <typename func, typename... Args>
+    requires std::invocable<func, Args...>
+  NTSTATUS SCall(uint64_t funcNameHash, Args &&...args) {
+    return trampoline(getSyscallNumber(funcNameHash), getSyscallInstrAddr(),
+                      getArgStackSize(args...), std::forward<Args>(args)...);
   }
 };
 
